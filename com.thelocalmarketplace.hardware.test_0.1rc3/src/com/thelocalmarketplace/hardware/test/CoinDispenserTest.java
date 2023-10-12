0650bd28bda927a6bee3cb16a205ccc8ae067494
sukhnaaz <sukhn@DESKTOP-5C0D03H>
2023-10-12 15:18:16 -0600

    2023 10 12

--- com.thelocalmarketplace.hardware.test_0.1rc3/src/com/thelocalmarketplace/hardware/test/CoinDispenserTest.java
+++ com.thelocalmarketplace.hardware.test_0.1rc3/src/com/thelocalmarketplace/hardware/test/CoinDispenserTest.java
@@ -11,7 +11,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.tdc.CashOverloadException;
+import com.tdc.ComponentFailure;
 import com.tdc.DisabledException;
+import com.tdc.NoCashAvailableException;
+import com.tdc.PassiveSource;
+import com.tdc.Sink;
 import com.tdc.coin.Coin;
 import com.tdc.coin.CoinDispenser;
 
@@ -51,6 +55,19 @@ public class CoinDispenserTest {
         }
         assertThrows(CashOverloadException.class, () -> dispenser.receive(coin1Dollar));
     }
+    
+    /** TEMP TESTING DELETE
+    @Test
+    public void testRejectValidCoin() throws DisabledException, CashOverloadException {
+        // Arrange
+        dispenser.receive(coin1Dollar); // Assuming receive method works properly
+
+        // Act
+        dispenser.reject(coin1Dollar);
+
+        // Assert: Implement assertions based on your test scenario
+        assertFalse(dispenser.isCoinInDispenser(coin1Dollar));
+    }**/
 
     @Test(expected = SimulationException.class)
     public void testNegativeCapacity() {
@@ -83,6 +100,20 @@ public class CoinDispenserTest {
         dispenser.load(coin1Dollar, coin25Cents);
         assertEquals(2, dispenser.size());
     }
+
+    @Test(expected = CashOverloadException.class)
+    public void testOverloadLoadCoins() throws CashOverloadException, NullPointerSimulationException {
+        dispenser.load(coin1Dollar, coin25Cents, coin25Cents);
+    }
+    
+    @Test
+    public void testUnload() throws NoPowerException, SimulationException, CashOverloadException {
+        dispenser.load(coin1Dollar , coin1Dollar , coin1Dollar); // Load 3 coins
+        int originalSize = dispenser.size();
+
+        dispenser.unload(); // Unload coins
+        assertEquals(originalSize, dispenser.getCapacity());
+    }
     
     @Test
     public void testUnloadCoins() throws SimulationException, CashOverloadException {
@@ -94,6 +125,73 @@ public class CoinDispenserTest {
     @Test(expected = NullPointerSimulationException.class)
     public void testLoadNullCoin() throws SimulationException, CashOverloadException {
         dispenser.load(coin1Dollar, null);
+    }
+    
+    @Test
+    public void testEmit() throws NoPowerException, DisabledException, CashOverloadException, NoCashAvailableException {
+        Sink<Coin> coinSinkStub = new Sink<Coin>() { // sink stub
+
+            @Override
+            public void receive(Coin cash) throws CashOverloadException, DisabledException {
+            }
+
+            @Override
+            public boolean hasSpace() {
+                return true;
+            }
+
+        };
+        dispenser.sink = coinSinkStub; // setup new sink
+
+        dispenser.load(coin1Dollar, coin1Dollar, coin1Dollar); // Load 3 $1 coins
+        int originalSize = dispenser.size();
+
+        dispenser.emit();
+        assertEquals(originalSize - 1, dispenser.size()); // check size decreased by 1
+    }
+    
+    @Test(expected = DisabledException.class)
+    public void testEmitDisabled() throws NoCashAvailableException, CashOverloadException, DisabledException {
+        dispenser.disable();
+        dispenser.emit();
+    }
+    
+    @Test(expected = NoCashAvailableException.class)
+    public void testEmitNoCashAvailable() throws CashOverloadException, NoCashAvailableException, DisabledException  {
+        dispenser.emit();
+    }
+    
+    @Test
+    public void testReject() throws DisabledException, ComponentFailure, CashOverloadException {
+        PassiveSource<Coin> cashSourceStub = new PassiveSource<Coin>() { // stub
+            
+			@Override
+			public void reject(Coin cash) throws CashOverloadException, DisabledException, ComponentFailure {
+				// TODO Auto-generated method stub
+
+			}
+        };
+
+        dispenser.source = cashSourceStub;
+        dispenser.reject(coin1Dollar);
+
+        // Assert behavior here
+    }
+    
+    @Test(expected = NoPowerException.class)
+    public void testRejectNoPower() throws DisabledException, CashOverloadException, NoPowerException {
+PassiveSource<Coin> cashSourceStub = new PassiveSource<Coin>() { // stub
+            
+			@Override
+			public void reject(Coin cash) throws CashOverloadException, DisabledException, ComponentFailure {
+				// TODO Auto-generated method stub
+
+			}
+        };
+
+        dispenser.source = cashSourceStub;
+        dispenser.disactivate();
+        dispenser.reject(coin1Dollar);
     }
 
 
